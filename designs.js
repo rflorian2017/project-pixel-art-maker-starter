c60e5e05a2966feac1e0fd1b0e419a3d4e5b5f36
rflorian2017 <roby_florian@yahoo.com>
2017-08-30 23:47:02 +0300

    First commit

--- designs.js
+++ designs.js
@@ -3,8 +3,50 @@
 
 // When size is submitted by the user, call makeGrid()
 
+var doc = this.document;
+var formSubmit = doc.getElementById('sizePicker');
+var pixelCanvas = doc.getElementById('pixel_canvas');
+var canvas = doc.createElement('canvas');
+var ctx = canvas.getContext('2d');
+var height = undefined;
+var width = undefined;
+
+formSubmit.addEventListener('submit', function(event){
+	event.preventDefault();
+	makeGrid();
+	//return false;
+});
+
+canvas.addEventListener('click', function(ev) {
+	var rect = canvas.getBoundingClientRect();
+	var positionX = Math.floor((event.clientX - rect.left)/15);
+	var positionY = Math.floor((event.clientY - rect.top)/15);
+	var color = doc.getElementById('colorPicker').value;
+	
+	ctx.fillStyle = color;
+	ctx.fillRect(positionX * 15, positionY * 15,15,15);
+	
+});
+
 function makeGrid() {
-
-// Your code goes here!
-
+	if((height !== undefined) && (width !== undefined)) {
+		ctx.clearRect(0,0,width,height + 6);
+	}
+	
+	height = doc.getElementById('input_height').value;
+	width = doc.getElementById('input_width').value;
+	
+	canvas.width = width * 15;
+    canvas.height = height * 15;
+	var row = pixelCanvas.insertRow(0);
+	var cell = row.insertCell(0);
+	cell.appendChild(canvas);
+	
+	for(var i = 0; i< width ; i++ )
+		for(var j = 0; j< height; j++)
+		{
+			ctx.strokeRect(i*15, j* 15 , 15, 15);
+		}
 }
+
+//makeGrid();
