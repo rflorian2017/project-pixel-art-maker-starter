4c9760b736396ab412091f73ba3898b964a8a02f
rflorian2017 <30376759+rflorian2017@users.noreply.github.com>
2017-08-31 16:04:52 +0300

    Add files via upload

--- designs.js
+++ designs.js
@@ -16,7 +16,7 @@ formSubmit.addEventListener('submit', function(event){
 
 function changeColor(cell) {
 	//select the color, directly in the event function
-	var color = doc.getElementById('colorPicker').value;
+	let color = doc.getElementById('colorPicker').value;
 	cell.bgColor = color; //change the background color
 }
 
@@ -25,18 +25,18 @@ function makeGrid() {
 	pixelCanvas.innerHTML = '';
 	
 	//read the height and the width
-	var height = doc.getElementById('input_height').value;
-	var width = doc.getElementById('input_width').value;
+	let height = doc.getElementById('input_height').value;
+	let width = doc.getElementById('input_width').value;
 	
 	//loop throughout the cells
-	for(var i = 0; i< height ; i++ )
+	for(let i = 0; i< height ; i++ )
 	{
 		//insert a row in the position defined by i
-		var row = pixelCanvas.insertRow(i);
-		for(var j = 0; j< width; j++)
+		let row = pixelCanvas.insertRow(i);
+		for(let j = 0; j< width; j++)
 		{
 			//insert a cell, in the position defined by j
-			var cell = row.insertCell(j);
+			let cell = row.insertCell(j);
 			//add event listener for clicking on a cell
 			cell.onclick = function() {
 				changeColor(this);
